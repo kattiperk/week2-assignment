c07b69aee9286fb8c783455337b83081389ff063
Kateryna Masiuk <kmasiuk@uw.edu>
2021-04-20 22:32:39 -0700

    adding .svg

--- blog-homepage/src/Article.js
+++ blog-homepage/src/Article.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import ArticleAuthor from './ArticleAuthor.js'
 import PropTypes from 'prop-types';
+import bookmark from './svg/bookmark.svg';
+import star from './svg/star.svg';
+import audio from './svg/volume.svg';
 
 class Article extends Component {
     static propTypes = {
@@ -17,17 +20,6 @@ class Article extends Component {
     }
     
     render() {
-        const audio = () => {
-            if (this.props.article.hasAudioAvailable === true) {
-                return `Audio availbale`
-            }
-        }
-
-        const member = () => {
-            if (this.props.article.memberPreview === true) {
-                return `Member preview`
-            }
-        }
 
 
         return (
@@ -37,10 +29,16 @@ class Article extends Component {
                         <a href={this.props.article.link}></a>
                     </div>
                     <div className="card-content-holder">
-                        <div className="card-content-features">
-                            <div className="card-content-audio">{audio()}</div>
-                            <div className="card-contet-member-preview">{member()}</div>
-                        </div>
+                            <div
+                                className={this.props.article.hasAudioAvailable ? 'card-content-audio is-visible' : 'card-content-audio is-invisible'}
+                            >
+                                Audio availbale
+                            </div>
+                            <div 
+                                className={this.props.article.memberPreview ? 'card-content-member is-visible' : 'card-content-member is-invisible'}
+                            >
+                                Member preview
+                            </div>
                         <div className="card-content-text-holder">
                             <h4><a href={this.props.article.link}>{this.props.article.title}</a></h4>
                             <p>{this.props.article.description}</p>
@@ -50,9 +48,7 @@ class Article extends Component {
                                 <ArticleAuthor author={this.props.article.author} article={this.props.article} />
                                 <div className="card-content-button">
                                     <a href="#">
-                                        <svg width="25" height="25" viewBox="0 0 25 25">
-                                            <path d="M19 6a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v14.66h.01c.01.1.05.2.12.28a.5.5 0 0 0 .7.03l5.67-4.12 5.66 4.13a.5.5 0 0 0 .71-.03.5.5 0 0 0 .12-.29H19V6zm-6.84 9.97L7 19.64V6a1 1 0 0 1 1-1h9a1 1 0 0 1 1 1v13.64l-5.16-3.67a.49.49 0 0 0-.68 0z" fill-rule="evenodd"></path>
-                                        </svg>
+                                        <img src={bookmark} alt="bookmark" />
                                     </a>
                                 </div>
                             </div>
